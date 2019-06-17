bcabc70f23e3bd4d659d9708dbecd903b8c02e15
Sakura2029 <519169644@qq.com>
2019-06-17 18:39:08 +0800

    Initial commit

---
+++ module01/src/com/module/Hello.java
@@ -0,0 +1,11 @@
+package com.module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        List s=new ArrayList<>();
+    }
+}
